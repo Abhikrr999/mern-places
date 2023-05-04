bfc7d783fa89acacb06cbe14b0991c93ecb223d5
abhi <abhi@windowspc.com>
2023-05-04 17:20:50 +0530

    some modifications

--- app.js
+++ app.js
@@ -5,33 +5,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-//const helmet = require("helmet");
+
 
 const app = express();
 
 
-//app.use(helmet());
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images'))); 
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//   next();
-// });
 app.use(cors());
-
-
-// app.use('/uploads/images', (req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   next();
-// }, express.static(path.join(__dirname, 'uploads', 'images')));
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/user-routes");
@@ -40,27 +24,6 @@ const HttpError = require("./models/http-error");
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
-
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route", 404);
-//   throw error;
-// });
-
-// app.use((error, req, res, next) => {
-//   // error middleware
-
-//   if (req.file.path) {
-//     fs.unlink(req.file.path, (err) => {
-//       console.log(err);
-//     });
-//   }
-
-//   if (res.headersSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occured" });
-// });
 
 app.use((error, req, res, next) => {
  
