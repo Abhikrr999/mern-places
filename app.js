c4d521b64b780285eaf6c4a53da0349e3f526fcb
abhi <abhi@windowspc.com>
2023-05-04 17:43:09 +0530

    some modifications III

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 const app = express();
+const helmet = require("helmet");
 
 app.use(bodyParser.json());
-app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
-// Enable CORS for all requests
+app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images'))); 
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+//   next();
+// });
+
 app.use(cors());
-
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/user-routes");
 
@@ -19,20 +34,42 @@ const HttpError = require("./models/http-error");
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route", 404);
+//   throw error;
+// });
+
+// app.use((error, req, res, next) => {
+//   // error middleware
+
+//   if (req.file.path) {
+//     fs.unlink(req.file.path, (err) => {
+//       console.log(err);
+//     });
+//   }
+
+//   if (res.headersSent) {
+//     return next(error);
+//   }
+//   res.status(error.code || 500);
+//   res.json({ message: error.message || "An unknown error occured" });
+// });
+
 app.use((error, req, res, next) => {
-  if (req.file && req.file.path) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
-  }
-
-  if (res.headersSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred" });
-});
-
+ 
+   if (req.file && req.file.path) {
+     fs.unlink(req.file.path, (err) => {
+       console.log(err);
+     });
+   }
+ 
+   if (res.headersSent) {
+     return next(error);
+   }
+   res.status(error.code || 500);
+   res.json({ message: error.message || "An unknown error occurred" });
+ });
+ 
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mapproject.ogkodq3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
