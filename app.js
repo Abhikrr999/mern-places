dd30a7c24f79459b7872f959aee7150c8152810b
abhi <abhi@windowspc.com>
2023-05-04 11:38:32 +0530

    some app modifications

--- app.js
+++ app.js
@@ -14,18 +14,15 @@ app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images'))); 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin , X-Requested-With, Content-Type, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   next();
 });
-
 
 
 const placesRoutes = require("./routes/places-routes");
