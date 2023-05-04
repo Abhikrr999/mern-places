cb0edae7050aa304c9b3c6e2589cae07da44aae7
abhi <abhi@windowspc.com>
2023-05-04 17:38:50 +0530

    some modifications II

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 const app = express();
 
-
 app.use(bodyParser.json());
+app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
-app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images'))); 
-
-
-app.use(cors({
-  origin:"https://favplacesonrender.com"
-}));
+// Enable CORS for all requests
+app.use(cors());
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/user-routes");
@@ -28,20 +20,19 @@ app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((error, req, res, next) => {
- 
-   if (req.file && req.file.path) {
-     fs.unlink(req.file.path, (err) => {
-       console.log(err);
-     });
-   }
- 
-   if (res.headersSent) {
-     return next(error);
-   }
-   res.status(error.code || 500);
-   res.json({ message: error.message || "An unknown error occurred" });
- });
- 
+  if (req.file && req.file.path) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+
+  if (res.headersSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred" });
+});
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mapproject.ogkodq3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
