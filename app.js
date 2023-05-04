d80fc1b58b3f5d93371c306edb94f10ac71368dd
abhi <abhi@windowspc.com>
2023-05-04 18:15:44 +0530

    some changes in CORS

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const cors = require("cors");
 
 
 const app = express();
-const helmet = require("helmet");
+
 
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images'))); 
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//   next();
-// });
+  next();
+});
 
-app.use(cors());
+//app.use(cors());
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/user-routes");
 
