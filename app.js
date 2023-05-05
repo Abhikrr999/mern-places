fd328d18b4c7eb9f0d27682e30400f0788105723
abhi <abhi@windowspc.com>
2023-05-05 09:18:20 +0530

    some changes in CORS handling

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images'))); 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-  next();
-});
+//   next();
+// });
 
-//app.use(cors());
+app.use(cors());
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/user-routes");
 
